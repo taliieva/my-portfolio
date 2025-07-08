e273d1bf110b855e72622f0c7b263e173053119e
008039 <008039@ibar.int>
2025-07-08 09:42:43 +0400

    update contact links

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -3,6 +3,7 @@ import { Calendar1, Github, Linkedin, Mail, Phone } from "lucide-react";
 import Image from "next/image";
 
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const MotionDiv = dynamic(
   () => import("framer-motion").then((mod) => mod.motion.div),
@@ -16,26 +17,31 @@ export default function ContactInfo() {
       icon: <Mail size={20} color="var(--foreground)" />,
       label: "Email",
       value: "teranealieva01@gmail.com",
+      href: "mailto:teranealieva01@gmail.com",
     },
     {
       icon: <Phone size={20} color="var(--foreground)" />,
       label: "Phone",
       value: "+994 50 755 01 30",
+      href: "tel:+994507550130",
     },
     {
       icon: <Calendar1 size={20} color="var(--foreground)" />,
       label: "Birthday",
       value: "1 December 2001",
+      href: '', 
     },
     {
       icon: <Linkedin size={20} color="var(--foreground)" />,
       label: "Linkedin",
-      value: "http://linkedin.com/in/tarana-aliyeva",
+      value: "linkedin.com/in/tarana-aliyeva",
+      href: "https://linkedin.com/in/tarana-aliyeva",
     },
     {
       icon: <Github size={20} color="var(--foreground)" />,
       label: "Github",
-      value: "https://github.com/taliieva",
+      value: "github.com/taliieva",
+      href: "https://github.com/taliieva",
     },
   ];
 
@@ -61,9 +67,9 @@ export default function ContactInfo() {
                 {info.label}
               </span>
             </div>
-            <p className="text-[color:var(--foreground)] text-[12px] md:text-sm break-words">
+            <Link href={info.href} className="text-[color:var(--foreground)] text-[12px] md:text-sm break-words">
               {info.value}
-            </p>
+            </Link>
           </MotionDiv>
         ))}
       </div>
